f2ebee0483b8a3feb5ff6f7eb782170a5a710d45
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-03-31 15:04:04 +0200

    creates a list in file ContactList and adds it as a component to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Contact from './components/Contact.js';
+import ContactList from './components/ContactList.js';
 
 function App() {
   return (
     <div>
-      <Contact name="Kristin Alexander" avatar="https://randomuser.me/api/portraits/women/66.jpg" online={true} />
-      <Contact name="Lonnie Penna" avatar="https://randomuser.me/api/portraits/men/44.jpg" online={false} />
-      <Contact name="April Herrera" avatar="https://randomuser.me/api/portraits/women/48.jpg" online={false} />
+      <ContactList />
     </div>
   );
 }
